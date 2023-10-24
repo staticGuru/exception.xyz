48ac160fa272b98e89e616a2c26aea4f23259390
staticGuru <vigneshguru274@gmail.com>
2023-10-24 22:08:31 +0530

    add sticky navbar

--- frontend/src/app/[lang]/components/Navbar.tsx
+++ frontend/src/app/[lang]/components/Navbar.tsx
@@ -68,7 +68,7 @@ export default function Navbar({
     setMobileMenuOpen(false);
   };
   return (
-    <div className="p-4 fixed w-full dark:bg-black dark:text-gray-100">
+    <div className="p-4 sticky bg-white top-0 w-full dark:bg-black dark:text-gray-100">
       <div className="container flex justify-between h-16 mx-auto px-0 sm:px-6">
         <Logo src={logoUrl}>
           {logoText && <h2 className="text-2xl font-bold">{logoText}</h2>}
