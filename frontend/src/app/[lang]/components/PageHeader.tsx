94de51d5db0edbb384da6f2ce90eab2dd0985630
staticGuru <vigneshguru274@gmail.com>
2023-10-26 22:22:23 +0530

    add the pageheader

--- frontend/src/app/[lang]/components/PageHeader.tsx
+++ frontend/src/app/[lang]/components/PageHeader.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-
+import { GiArtificialIntelligence } from "react-icons/gi";
 interface PageHeaderProps {
-  heading: string,
-  text?: string,
+  heading: string;
+  text?: string;
 }
 
-export default function PageHeader({ heading, text } : PageHeaderProps) {
+export default function PageHeader({ heading, text }: PageHeaderProps) {
   return (
-    <div className="my-16 w-full text-center">
-    { text && <span className="text-violet-400 font-bold">{text}</span> }
-    <h2 className="text-4xl my-4 lg:text-5xl font-bold font-heading">{heading}</h2>
-  </div>
+    <div className="container my-14 px-12 text-left">
+      {text && <span className="text-slate-500 font-bold">{text}</span>}
+      <div className="flex items-center justify-start">
+        <h2 className="text-4xl my-2 mr-2 lg:text-5xl font-bold font-heading text-pink-500">
+          {heading}
+        </h2>
+        <GiArtificialIntelligence color="#facd13" size="2.5em"/>
+      </div>
+    </div>
   );
 }
