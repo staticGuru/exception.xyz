681b3e1c11835ffd5bdc984a48c88a8f5a68414d
staticGuru <vigneshguru274@gmail.com>
2023-11-06 13:29:57 +0530

    add the types

--- frontend/src/app/[lang]/utils/model.ts
+++ frontend/src/app/[lang]/utils/model.ts
@@ -4,68 +4,118 @@ type StrapiResponse<T> = {
 };
 
 export interface Attribute {
-	url: string;
-	alternativeText?: any;
-	caption?: any;
-	width: number;
-	height: number;
+  url: string;
+  alternativeText?: any;
+  caption?: any;
+  width: number;
+  height: number;
 }
 
 export interface Data {
-	id: number;
-	attributes: Attribute;
+  id: number;
+  attributes: Attribute;
 }
 
 export interface Picture {
-	data: Data;
+  data: Data;
 }
 
 export interface Button {
-	id: number;
-	url: string;
-	newTab: boolean;
-	text: string;
-	type: string;
+  id: number;
+  url: string;
+  newTab: boolean;
+  text: string;
+  type: string;
 }
 
 export interface ContentSection {
-	id: number;
-	__component: string;
-	title: string;
-	description: string;
-	picture: Picture;
-	buttons: Button[];
+  id: number;
+  __component: string;
+  title: string;
+  description: string;
+  picture: Picture;
+  buttons: Button[];
 }
 
 export interface Attribute {
-	shortName: string;
-	slug: string;
-	createdAt: string;
-	updatedAt: string;
-	publishedAt: string;
-	locale: string;
-	heading?: any;
-	description?: any;
-	contentSections: ContentSection[];
+  shortName: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  locale: string;
+  heading?: any;
+  description?: any;
+  contentSections: ContentSection[];
 }
 
 export interface Data {
-	id: number;
-	attributes: Attribute;
+  id: number;
+  attributes: Attribute;
 }
 
 export interface Pagination {
-	page: number;
-	pageSize: number;
-	pageCount: number;
-	total: number;
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
 }
 
 export interface Meta {
-	pagination: Pagination;
+  pagination: Pagination;
 }
 
 export interface RootObject {
-	data: Data[];
-	meta: Meta;
+  data: Data[];
+  meta: Meta;
+}
+export interface Article {
+  id: 4;
+  attributes: {
+    title: string;
+    description: string;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    cover: {
+      data: {
+        attributes: {
+          url: string;
+        };
+      };
+    };
+    category: {
+      data: {
+        attributes: {
+          name: string;
+          slug: string;
+        };
+      };
+    };
+    authorsBio: {
+      data: {
+        attributes: {
+          name: string;
+          avatar: {
+            data: {
+              attributes: {
+                url: string;
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
+export interface Topics {
+  attributes: {
+    name: string;
+    slug: string;
+    articles: {
+      data: Array<any>[];
+    };
+  };
 }
